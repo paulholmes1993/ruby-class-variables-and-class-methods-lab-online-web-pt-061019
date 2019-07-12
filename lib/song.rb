efd1c687e3c19e31242949afaa4d5db2226c5975
Paul Holmes <paulholmes1993@gmail.com>
2019-07-12 22:06:43 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+
+    attr_reader :artist, :genre
+
+    def initialize(name, artist=nil, genre=nil)
+     @name = name
+     self.artist = artist if artist != nil
+     self.genre = genre if genre != nil
+   end
+
+    def self.create(name)
+     instance = Song.new(name)
+     instance.save
+     instance
+   end
+
+    def artist=(artist)
+     @artist = artist
+     self.artist.add_song(self)
+   end
+
+    def genre=(genre)
+     @genre = genre
+     self.genre.add_song(self)
+   end
