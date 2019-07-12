fc09c938fa53ca7ac1d5a871241c85123619ef3a
Paul Holmes <paulholmes1993@gmail.com>
2019-07-12 22:02:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,62 +1,25 @@
-class Song
-   attr_accessor :name, :artist, :genre
+class Song < Super
 
-    @@all = []
+    attr_reader :artist, :genre
 
-    def initialize(name, artist = nil, genre = nil)
+    def initialize(name, artist=nil, genre=nil)
      @name = name
-     self.artist=artist if artist
-     self.genre=genre if genre
-   end
-
-    def self.all
-     @@all
-   end
-
-    def self.destroy_all
-     @@all.clear
-   end
-
-    def save
-     @@all << self
+     self.artist = artist if artist != nil
+     self.genre = genre if genre != nil
    end
 
     def self.create(name)
-     song = self.new(name)
-     song.save
-     song
+     instance = Song.new(name)
+     instance.save
+     instance
    end
 
     def artist=(artist)
      @artist = artist
-     artist.add_song(self)
+     self.artist.add_song(self)
    end
 
     def genre=(genre)
      @genre = genre
-     if !(genre.songs.include?(self))
-       genre.songs << self
-     end
+     self.genre.add_song(self)
    end
-
-    def self.find_by_name(name)
-     all.detect {|song| song.name == name}
-   end
-
-    def self.find_or_create_by_name(name)
-     find_by_name(name) || create(name)
-   end
-
-    def self.new_from_filename(filename)
-     info = filename.split(" - ")
-     artist, name, genre = info[0], info[1], info[2].gsub( ".mp3" , "")
-     genre = Genre.find_or_create_by_name(genre)
-     artist = Artist.find_or_create_by_name(artist)
-
-      new(name,artist,genre)
-   end
-
-    def self.create_from_filename(filename)
-     new_from_filename(filename).tap{ |s| s.save}
-    end
- end
